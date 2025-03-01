af1ee19ff87496516995a52ca5dc6dd95a33f4c3
Vladimir Mitrev <orionsbelt1017@gmail.com>
2025-03-01 13:02:34 +0200

    feat - Vladimir - add cv routes

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import authController from '../controllers/auth.controller';
+import cvController from '../controllers/cv.controller';
 import { verifyToken } from '../middlewares/verifyToken';
 
 const router = express.Router();
@@ -17,5 +18,14 @@ router.post("/verify-email", authController.verifyEmail);
 router.post("/reset-password/:token", authController.resetPassword);
 
 router.post("/forgot-password", authController.forgotPassword);
+
+router.get('/', cvController.getMyCV );
+
+router.post('/create', cvController.createCV );
+
+router.post('/edit/:cvId', cvController.createCV );
+
+
+router.delete('/:cvId', cvController.deleteCV );
 
 export default router;
