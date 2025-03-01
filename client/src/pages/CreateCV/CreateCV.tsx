333955b76f24d4b2525d3606a20dc3ec6f332b5c
Viktor Stefanov <victorystefanov@gmail.com>
2025-03-01 14:46:12 +0200

    fix logic for the next stage

--- client/src/pages/CreateCV/CreateCV.tsx
+++ client/src/pages/CreateCV/CreateCV.tsx
@@ -17,13 +17,8 @@ const CreateCV: React.FC = () => {
   return (
     <div>
         {step === 1 && <h1><TechStackSection /></h1>}
-        {step === 2 && <h1><TechStackSection /></h1>}
+        {step === 2 && <h1><TechStackSection nextStep={nextStep} prevStep={prevStep} /></h1>}
         {step === 3 && <h1>Step 3</h1>}
-
-        <div className="mt-4">
-            {step > 1 && <button className="mx-4 bg-red-500 rounded-2xl w-16 h-8 text-white" onClick={prevStep}>Back</button>}
-            {step < 3 && <button className="mx-4 bg-red-500 rounded-2xl w-16 h-8 text-white" onClick={nextStep}>Next</button>} 
-        </div>
 
     </div>
   )
