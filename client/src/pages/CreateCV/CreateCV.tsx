246f5e9b3dcce4975a5dbb7a942868b82e3de9e8
atanasios <atanasgeorgiev444@gmail.com>
2025-03-01 15:32:53 +0200

    fix merge

--- client/src/pages/CreateCV/CreateCV.tsx
+++ client/src/pages/CreateCV/CreateCV.tsx
@@ -15,15 +15,9 @@ const CreateCV: React.FC = () => {
     };
 
   return (
-<<<<<<< HEAD
-    <div className="w-full">
-        {step === 1 && <h1><AboutMe onSubmit={onSubmit}/></h1>}
-        {step === 2 && <h1>Step 2</h1>}
-=======
     <div>
         {step === 1 && <h1><AboutMeSection nextStep={nextStep} /></h1>}
         {step === 2 && <h1><TechStackSection nextStep={nextStep} prevStep={prevStep} /></h1>}
->>>>>>> 76660423f531970f2ec0087a4a03c3f635a156ce
         {step === 3 && <h1>Step 3</h1>}
 
     </div>
